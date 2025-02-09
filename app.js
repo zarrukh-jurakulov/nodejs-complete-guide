28358d819de2a8e292dd03b82c77410cd16ba569
zarrukh <zarrukhjurakulov474@gmail.com>
2025-02-09 16:58:03 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  res.setHeader("Content-Type", "text/html");
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' /><button type='submit'>Submit</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.writeHead("Location", "/");
+    return res.end();
+  }
+
+  res.end();
+});
+
+server.listen(3000);
