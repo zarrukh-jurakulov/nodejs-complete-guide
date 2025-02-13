1a9bef88a96d375df842b113d3e1991d2038957c
zarrukh <z.jurakulov@uzinfocom.uz>
2025-02-13 21:03:09 +0500

    added: body-parser

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const http = require("http");
-
 const express = require("express");
-
-const routes = require("./routes");
+const bodyParser = require("body-parser")
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`
+    <form action="/product" method="POST">
+        <input type="text" name="title" />
+        <button type="submit">Add Product</button>
+    </form>
+    `)
 });
 
-app.use((req, res, next) => {
-  console.log("In another middleware");
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express</h1>')
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000)
